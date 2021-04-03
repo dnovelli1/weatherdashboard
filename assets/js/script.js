8c0b4729d6be1bcfa3cca866f87397014ddf79ab
Jake Novelli <jakenovelli11@gmail.com>
2021-04-02 20:44:09 -0700

    added 2 global variables and listed needed inputs for on click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+// Needed Variables
+
+var searchButton = $('.searchButton');
+var apiKey = "0e5c724f73721354b141f5525b352662";
+
+
+// Function to run on search button click
+searchButton.on('click', function() {
+
+    // Grabs search input value
+    var searchInput = $('.searchInput').val();
+    // URL for today's forecast and concatinating search input into text box and my api key
+    urlToday = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + apiKey + "&units=imperial";
+    // URL for 5 day forecast and concatinating search input into text box and my api key
+    urlFive = "api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + apiKey + "&units=imperial";
+})
